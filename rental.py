3d7d5b64c6ea525aa1ce0041b297a5989a9c574a
Jakarin Chujan <jojogamer67@gmail.com>
2021-10-17 01:07:29 +0700

    :recycle: Extract Method from statement() to calculate_rental()

--- rental.py
+++ rental.py
@@ -1,5 +1,5 @@
 class Rental:
-	"""
+    """
 	A rental of a movie by customer.
 	From Fowler's refactoring example.
 
@@ -9,16 +9,16 @@ class Rental:
 	But for simplicity of the example only a days_rented
 	field is used.
 	"""
-	
-	def __init__(self, movie, days_rented): 
-		"""Initialize a new movie rental object for
+
+    def __init__(self, movie, days_rented):
+        """Initialize a new movie rental object for
 		   a movie with known rental period (daysRented).
 		"""
-		self.movie = movie
-		self.days_rented = days_rented
+        self.movie = movie
+        self.days_rented = days_rented
 
-	def get_movie(self):
-		return self.movie
+    def get_movie(self):
+        return self.movie
 
-	def get_days_rented(self):
-		return self.days_rented
+    def get_days_rented(self):
+        return self.days_rented
