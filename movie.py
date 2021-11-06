fdcb7a62ffa88b3ddb8fa847675ec5dacd48b57b
Jakarin Chujan <jojogamer67@gmail.com>
2021-11-07 00:43:12 +0700

    :label: Add type for method in movie.py

--- movie.py
+++ movie.py
@@ -2,21 +2,6 @@ from typing import List
 import pandas as pd
 
 
-class MovieCatalog:
-    """A movie catalog for renting movies."""
-
-    def __init__(self):
-        self.movie_list = pd.read_csv("movies.csv")
-
-    def get_movie(self, title):
-        """Get a movie from a movie.csv"""
-        movie = self.movie_list[self.movie_list['title'] == title]
-        movie_title = movie.iloc[0]['title']
-        year = int(movie.iloc[0]['year'])
-        genre = movie.iloc[0]['genres'].split('|')
-        return Movie(movie_title, year, genre)
-
-
 class Movie:
     """A movie available for rent."""
 
@@ -29,13 +14,13 @@ class Movie:
     def get_title(self) -> str:
         return self._title
 
-    def get_year(self):
+    def get_year(self) -> int:
         return self._year
 
-    def get_genre(self):
+    def get_genre(self) -> List[str]:
         return self._genre
 
-    def is_genre(self, string):
+    def is_genre(self, string) -> bool:
         """Returns: true if the string parameter matches one of the movie’s genre."""
         if string in self._genre:
             return True
@@ -43,3 +28,18 @@ class Movie:
 
     def __str__(self) -> str:
         return self._title
+
+
+class MovieCatalog:
+    """A movie catalog for renting movies."""
+
+    def __init__(self):
+        self.movie_list = pd.read_csv("movies.csv")
+
+    def get_movie(self, title: str) -> Movie:
+        """Get a movie from a movie.csv"""
+        movie = self.movie_list[self.movie_list['title'] == title]
+        movie_title = movie.iloc[0]['title']
+        year = int(movie.iloc[0]['year'])
+        genre = movie.iloc[0]['genres'].split('|')
+        return Movie(title=movie_title, year=year, genre=genre)
