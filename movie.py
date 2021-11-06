74bafe51a455ce844260bfe4f52f7a805c2b4728
Jakarin Chujan <jojogamer67@gmail.com>
2021-11-06 22:53:30 +0700

    :truck: PriceCode

--- movie.py
+++ movie.py
@@ -1,39 +1,11 @@
-from enum import Enum
-
-
-class PriceCode(Enum):
-    """An enumeration for different kinds of movies and their behavior"""
-    new_release = {"price": lambda days: 3.0 * days,
-                   "frp": lambda days: days
-                   }
-    regular = {"price": lambda days: 2 if days <= 2 else 2 + 1.5 * (days - 2),
-               "frp": lambda days: 1
-               }
-    childrens = {"price": lambda days: 1.5 if days <= 3 else 1.5 + 1.5 * (days - 3),
-                 "frp": lambda days: 1
-                 }
-
-    def price(self, days: int) -> float:
-        "Return the rental price for a given number of days"""
-        pricing = self.value["price"]  # the enum member's price formula
-        return pricing(days)
-
-    def point(self, day: int) -> float:
-        pointing = self.value["frp"]
-        return pointing(day)
 
 
 class Movie:
     """A movie available for rent."""
 
-    def __init__(self, title: str, price_code: PriceCode):
+    def __init__(self, title: str):
         # Initialize a new movie.
         self.title = title
-        self.price_code = price_code
-
-    def get_price_code(self) -> PriceCode:
-        # get the price code
-        return self.price_code
 
     def get_title(self) -> str:
         return self.title
--- rental.py
+++ rental.py
@@ -1,5 +1,28 @@
+from enum import Enum
 from movie import *
 import logging
+
+
+class PriceCode(Enum):
+    """An enumeration for different kinds of movies and their behavior"""
+    new_release = {"price": lambda days: 3.0 * days,
+                   "frp": lambda days: days
+                   }
+    regular = {"price": lambda days: 2 if days <= 2 else 2 + 1.5 * (days - 2),
+               "frp": lambda days: 1
+               }
+    childrens = {"price": lambda days: 1.5 if days <= 3 else 1.5 + 1.5 * (days - 3),
+                 "frp": lambda days: 1
+                 }
+
+    def price(self, days: int) -> float:
+        "Return the rental price for a given number of days"""
+        pricing = self.value["price"]  # the enum member's price formula
+        return pricing(days)
+
+    def point(self, day: int) -> float:
+        pointing = self.value["frp"]
+        return pointing(day)
 
 
 class Rental:
@@ -13,10 +36,11 @@ class Rental:
 	But for simplicity of the example only a days_rented field is used.
 	"""
 
-    def __init__(self, movie: Movie, days_rented: int):
+    def __init__(self, movie: Movie, days_rented: int, price_code: PriceCode):
         """Initialize a new movie rental object for a movie with known rental period (daysRented)."""
         self.movie = movie
         self.days_rented = days_rented
+        self.price_code = price_code
 
     def get_movie(self) -> Movie:
         return self.movie
@@ -28,7 +52,7 @@ class Rental:
         return self.days_rented
 
     def get_charge(self) -> float:
-        return self.get_movie().get_price_code().price(self.days_rented)
+        return self.price_code.price(self.days_rented)
 
     def get_frequent_rental_points(self) -> float:
-        return self.get_movie().get_price_code().point(self.days_rented)
+        return self.price_code.point(self.days_rented)
